652676e122d4be6df474c85d3648745766579ab6
MondaleFelix <mondalefelix@yahoo.com>
2020-07-12 11:38:56 -0700

    added Project and Task model

---
+++ TaskeeApp/TaskeeApp/Model/Project.swift
@@ -0,0 +1,15 @@
+//
+//  Project.swift
+//  TaskeeApp
+//
+//  Created by Mondale on 7/12/20.
+//  Copyright © 2020 Mondale. All rights reserved.
+//
+
+import UIKit
+
+struct Project {
+    var name: String
+    var color: UIColor
+    var tasksList: [Task]
+}
---
+++ TaskeeApp/TaskeeApp/Model/Task.swift
@@ -0,0 +1,16 @@
+//
+//  Task.swift
+//  TaskeeApp
+//
+//  Created by Mondale on 7/12/20.
+//  Copyright © 2020 Mondale. All rights reserved.
+//
+
+import Foundation
+
+struct Task {
+    let project: Project
+    var title: String
+    var dueDate: String
+    var status: Bool
+}
